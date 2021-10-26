e87ad2d7020779a9874edfd1a583a2715bfc41cf
Daniel Fox <dfxamzn@amazon.com>
2021-10-26 12:46:10 -0600

    add traces

--- maisey_service/app.js
+++ maisey_service/app.js
@@ -1,8 +1,14 @@
+var AWSXRay = require('aws-xray-sdk');
 
 exports.handler = async () => {
 
     const answer = { message: "Maisey is a GOOD dog." };
 
+    const segment = AWSXRay.getSegment(); //returns the facade segment
+    const subsegment = segment.addNewSubsegment('sleeping!');
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    subsegment.close();
+    
     const response = {
         statusCode: 200,
         body: JSON.stringify(answer)
